ca1925cd4b90c2c37a3c400776575a4aed90186e
shyam g <meghashyam.g@gmail.com>
2022-03-14 10:52:30 +0530

    deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
+  const [tableView, setView] = useState(true);
   return (
     <div className="App">
-      <button className="toggleForm">
+      <button className="toggleForm" onClick={() => {
+        setView(!tableView)
+      }}>
         {/* Show text Add House or Show Rentals based on state */}
+        {tableView ? "Add House" : "Show Rentals"}
       </button>
       {/* Show component based on state */}
+      {tableView ? <Rentals/> : <AddHouse/>}
       <br />
     </div>
   );
--- src/Components/AddHouse/AddHouse.jsx
+++ src/Components/AddHouse/AddHouse.jsx
@@ -1,34 +1,67 @@
+import axios from "axios";
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
 export const AddHouse = () => {
+  let [formdata, setFormdata] = useState({
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    bachelor: "",
+    married: "",
+    image: ""
+  })
+  const handleChange = (e) => {
+    console.log(e.target);
+    const [className, value] = e.target;
+    setFormdata({
+      ...formdata, [className] : value
+    })
+    console.log(formdata);
+  }
+
+  const submitForm = () => {
+    console.log(formdata);
+    axios
+    .post(`http://localhost:8080/houses`, {
+      ...formdata, id: nanoid()
+    })
+  }
+
   return (
     <div className="addHouseContainer">
       <form>
         <label>name</label>
-        <input type="text" className="name" value={""} required />
+        <input type="text" className="name" value={""} onChange={handleChange} required />
         <br />
         <label>ownerName</label>
-        <input value={""} type="text" className="ownerName" required />
+        <input value={""} type="text" className="ownerName" onChange={handleChange} required />
         <br />
         <label>address</label>
-        <input value={""} type="text" className="address" required />
+        <input value={""} type="text" className="address" required onChange={handleChange}/>
         <br />
         <label>areaCode</label>
-        <input value={""} type="text" className="areaCode" required />
+        <input value={""} type="text" className="areaCode" required onChange={handleChange}/>
         <br />
         <label>rent</label>
-        <input value={""} type="text" className="rent" required />
+        <input value={""} type="text" className="rent" required onChange={handleChange}/>
         <br />
         <label>preferredTenant</label>
         <br />
         <label>bachelor</label>
-        <input checked={""} type="checkbox" className="bachelor" />
+        <input checked={""} type="checkbox" className="bachelor" onChange={handleChange}/>
         <br />
         <label>married</label>
-        <input checked={""} type="checkbox" className="married" />
+        <input checked={""} type="checkbox" className="married" onChange={handleChange}/>
         <br />
         <label>image</label>
-        <input value={""} type="text" className="image" required />
+        <input value={""} type="text" className="image" required onChange={handleChange}/>
         <br />
-        <input className="submitBtn" type="submit" />
+        <input className="submitBtn" type="submit" onClick={() => {
+          submitForm()
+        }}/>
       </form>
     </div>
   );
